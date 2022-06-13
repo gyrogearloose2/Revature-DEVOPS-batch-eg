ac14c1b03393115ee856d2e563c3d055a1decec8
Eric Griffin <griffe1154@yahoo.com>
2022-06-12 21:29:01 -0700

    A quick test file to confirm git is working as expected

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def calculator(a,b,c):
+    """starting form of a calculator takes in command line inputs a,b and c
+    then performs operation b on a and c. This is just enough to test out 
+    the git repository
+    """
+    print (a,b,c)
+    return
